7fbbb7bd1e3a5e263a52c3e69db1d551c1bd4b97
apple-yagi <applesryu@gmail.com>
2022-07-18 17:16:53 +0900

    Apply emotion

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,16 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react({
+      jsxImportSource: "@emotion/react",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+      },
+    }),
+    tsconfigPaths(),
+  ],
   esbuild: {
-    jsxFactory: "jsx",
-    jsxInject: "import { jsx } from '@emotion/react'",
+    logOverride: { "this-is-undefined-in-esm": "silent" },
   },
 });
